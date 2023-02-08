08ecfcdd2174c5947cce9a03ae974814c3f16aea
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-02-07 21:11:00 -0600

    Update receiver.js

--- s/webrtc-simple-datachannel/receiver.js
+++ s/webrtc-simple-datachannel/receiver.js
@@ -39,12 +39,12 @@
   function connectPeers() {
     // Create the local connection and its event listeners
     
-    localConnection = new RTCPeerConnection();
+    //localConnection = new RTCPeerConnection();
     
     // Create the data channel and establish its event listeners
-    sendChannel = localConnection.createDataChannel("sendChannel");
-    sendChannel.onopen = handleSendChannelStatusChange;
-    sendChannel.onclose = handleSendChannelStatusChange;
+    //sendChannel = localConnection.createDataChannel("sendChannel");
+   // sendChannel.onopen = handleSendChannelStatusChange;
+    //sendChannel.onclose = handleSendChannelStatusChange;
     
     // Create the remote connection and its event listeners
     
@@ -53,22 +53,18 @@
     
     // Set up the ICE candidates for the two peers
     
-    localConnection.onicecandidate = e => !e.candidate
-        || remoteConnection.addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
+
+
 
     remoteConnection.onicecandidate = e => !e.candidate
-        || localConnection.addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
+          || remoteConnection.addIceCandidate(e.candidate)
+        .catch(handleAddCandidateError);     
     
     // Now create an offer to connect; this starts the process
     
-    localConnection.createOffer()
-    .then(offer => localConnection.setLocalDescription(offer))
-    .then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
+remoteConnection.setRemoteDescription(remoteConnection.localDescription)
     .then(() => remoteConnection.createAnswer())
     .then(answer => remoteConnection.setLocalDescription(answer))
-    .then(() => localConnection.setRemoteDescription(remoteConnection.localDescription))
     .catch(handleCreateDescriptionError);
   }
     
